6e3969f2745f8a254c8172bf4dcf5c79173ee6f1
Rohit <rohitgupta7471@gmail.com>
2024-03-11 20:51:53 +0530

    made changes

--- book-selling/src/components/Footer.jsx
+++ book-selling/src/components/Footer.jsx
@@ -1,23 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 function Footer() {
     return (
-        <div className="container">
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p className="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
-
-                <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
-                </a>
-
-                <ul className="nav col-md-4 justify-content-end">
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
-                </ul>
-            </footer>
-        </div>
+       
+        <div class="container">
+             <hr />
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+          </ul>
+          <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
+        </footer>
+      </div>
     )
 }
 export default Footer
--- book-selling/src/components/Home.jsx
+++ book-selling/src/components/Home.jsx
@@ -31,39 +31,110 @@ function Home() {
     ]
     return (
         <div className="home-container">
-            <div id="carouselExampleIndicators" class="carousel slide"  >
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <div id="carouselExampleIndicators" className="carousel slide"  >
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner" >
-                    <div class="carousel-item active">
-                        <img src={bookImage4} class="d-block w-100" alt="..." />
+                <div className="carousel-inner" >
+                    <div className="carousel-item active">
+                        <img src={bookImage4} className="d-block w-100" alt="..." />
                     </div>
-                    <div class="carousel-item">
-                        <img src={bookImage1} class="d-block w-100" alt="..." />
+                    <div className="carousel-item">
+                        <img src={bookImage1} className="d-block w-100" alt="..." />
                     </div>
-                    <div class="carousel-item">
-                        <img src={bookImage2} class="d-block w-100" alt="..." />
+                    <div className="carousel-item">
+                        <img src={bookImage2} className="d-block w-100" alt="..." />
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
                 </button>
             </div>
-            <h1 className="display-3 p-3 mx-3 my-1">Categories</h1>
+
+            <h4 className="display-5 p-3 mx-1 my-1">Categories</h4>
             <div className="categories-box" >
                 {
                     books.map((book, index) => (
                         <Card key={index} img={book.img} title={book.title}></Card>
                     ))
                 }
+            </div>
+
+            <h4 className="display-5 p-3 mx-1 my-1">Top-Seller</h4>
+
+            <div className="categories-box">
+            <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/81Ls+SBCLiL._SY466_.jpg" alt="" />
+                    <h4>Atomic Habits</h4>
+                    <h6>&gt; James Clear</h6>
+                    <h5>₹500</h5>
+                </div>
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/81n6auLS9aS._SY466_.jpg" alt="" />
+                    <h4>Three Hours</h4>
+                    <h6>&gt; Rosamund Lupton</h6>
+                    <h5>₹384</h5>
+                </div>
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/41E+2EfN3NL._SY445_SX342_.jpg" alt="" />
+                    <h4>The Psychology of Money</h4>
+                    <h6>&gt;Morgan Housel</h6>
+                    <h5>₹299</h5>
+                </div>
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/81wiMPTSFuL._SY466_.jpg" alt="" />
+                    <h4>Shri Ram Charitmanas</h4>
+                    <h6>&gt;Gita Press Gorakhpur</h6>
+                    <h5>₹440</h5>
+                </div>
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/410SBqmsPdL._SY445_SX342_.jpg" alt="" />
+                    <h4>The 5 AM Club</h4>
+                    <h6>&gt;Robin Sharma</h6>
+                    <h5>₹240</h5>
+                </div>
+            </div>
+
+
+            <h4 className="display-5 p-3 mx-1 my-1">Recommended</h4>
+            <div className="categories-box">
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/41-tZGcVRkL._SX342_SY445_.jpg" alt="" />
+                    <h4>Hindu Mythology - Vedic and Puranic</h4>
+                    <h6>&gt; William J. Wilkins</h6>
+                    <h5>₹249</h5>
+                </div>
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/410bFYkn9qL._SX342_SY445_.jpg" alt="" />
+                    <h4>Yoga & Stress Management </h4>
+                    <h6>&gt; Acharya Yatendra </h6>
+                    <h5>₹159</h5>
+                </div>
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/71vllLbpsdL._SY466_.jpg" alt="" />
+                    <h4>The Intelligent Investor</h4>
+                    <h6>&gt;  Benjamin Graham </h6>
+                    <h5>₹340</h5>
+                </div>
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/41TyduBWCdL._SY445_SX342_.jpg" alt="" />
+                    <h4>Meditations </h4>
+                    <h6>&gt; Marcus Aurelius</h6>
+                    <h5>₹149</h5>
+                </div>
+                <div className="card-container">
+                    <img src="https://m.media-amazon.com/images/I/81n6auLS9aS._SY466_.jpg" alt="" />
+                    <h4>Three Hours</h4>
+                    <h6>&gt; Rosamund Lupton</h6>
+                    <h5>₹384</h5>
+                </div>
             </div>
         </div>
     )
