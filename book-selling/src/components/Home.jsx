9e17777959789bcc72106ec260bd6bf7be6b5a55
Rohit <rohitgupta7471@gmail.com>
2024-03-11 15:42:45 +0530

    made changes

--- book-selling/src/components/Home.jsx
+++ book-selling/src/components/Home.jsx
@@ -1,5 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import bookImage from '../images/book2.jpg'
+import bookImage1 from '../images/book2.jpg'
+import bookImage2 from '../images/book3.jpg'
+import bookImage4 from '../images/book4.jpg'
+
+
 import Card from './Card';
 
 function Home() {
@@ -18,16 +22,41 @@ function Home() {
         },
         {
             img: "https://m.media-amazon.com/images/I/71pi-C7-oKL._AC_UY327_FMwebp_QL65_.jpg",
-            title: "Health/Fitness"
+            title: "Health"
         },
         {
             img: "https://m.media-amazon.com/images/I/91RTk4zk+QL._AC_UY327_FMwebp_QL65_.jpg",
-            title: "Spirituality"
+            title: "Religious"
         }
     ]
     return (
         <div className="home-container">
-            <img src={bookImage} className="img-fluid" style={{ objectFit: 'cover', maxHeight: '400px', width: '100%' }} alt="..." />
+            <div id="carouselExampleIndicators" class="carousel slide"  >
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner" >
+                    <div class="carousel-item active">
+                        <img src={bookImage2} class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={bookImage1} class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={bookImage4} class="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
             <h1 className="display-3 p-3 mx-3 my-1">Categories</h1>
             <div className="categories-box" >
                 {
